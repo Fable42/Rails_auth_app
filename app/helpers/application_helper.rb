83e3d541df5a7c8899f73a770b13750e3b82106e
Fable42 <kovalskii.4@mail.ru>
2024-02-26 18:07:10 +0300

    Added basic css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def wrapper_class
+    if controller_name == 'sessions' or controller_name == 'users' && action_name == 'show'
+      'wrapper_sign_in_up'
+    else
+      'wrapper'
+    end
+  end
+  
 end
